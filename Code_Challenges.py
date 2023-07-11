1524be38091a37d9f0900740d4ca2f47b20a28e4
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-07-11 13:45:22 -0500

    Update Code_Challenges.py
    Added additional challenge & solution

--- Code_Challenges.py
+++ Code_Challenges.py
@@ -132,3 +132,16 @@ strings = ['elb', 'isn', 'ehe', 'rpm', 'ocn', 'i']
 
 # ____________________________________
 
+# Challenge: Return the sum of the numbers in the array, except ignore sections of numbers 
+# starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+# Examples:
+    # summer_69([1, 3, 5]) --> 9
+    # summer_69([4, 5, 6, 7, 8, 9]) --> 9
+    # summer_69([2, 1, 6, 9, 11]) --> 14
+
+def summer_69(arr):
+    if 6 in arr:
+        del arr[arr.index(6):(arr.index(9))+1]
+    return sum(arr)
+
+# ____________________________________
